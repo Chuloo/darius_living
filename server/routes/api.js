23af1a48a58149272e916eef6c15585e84d8e305
Nwamadi Marcel <nwamadimarcel@gmail.com>
2017-09-01 00:16:03 +0100

    created connection to database

--- server/routes/api.js
+++ server/routes/api.js
@@ -28,29 +28,12 @@ router.get('/user/:id', function (req, res, next) {
 });
 
 
-router.get('/reviews', function(req, res, next){
-  db.users.find(function (err, users) {
-    if(err){
-      res.send(err);
-    }
-    res.json(users);
-  })
-});
 
-//Get Single visitor
-router.get('/reviews/:id', function (req, res, next) {
-	db.users.findOne({_id:mongojs.ObjectId(req.params.id)}, function(err, user){
-			if(err){
-				res.send(err);
-			}
-			res.json(user);
-	});
-});
 
 //add user
 router.post('/user', function(req, res, next){
 		var user = req.body;
-		if(!user.first_name || !(user.last_name + '') || !(user.specialty + '') || !(user.likes + '')){
+		if(!user.first_name || !(user.last_name + '') || !(user.phone_number + '') || !(user.appartment_amount + '') || !(user.date_from + '') || !(user.date_to + '')){
 				res.status(400);
 				res.json({
 					"error":"bad data"
--- src/app/book/book.component.spec.ts
+++ src/app/book/book.component.spec.ts
@@ -1,25 +1,94 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, OnInit } from '@angular/core';
+import {User} from '../user'
+import {UserService} from '../services/user.service';
 
-import { BookComponent } from './book.component';
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent implements OnInit {
 
-describe('BookComponent', () => {
-  let component: BookComponent;
-  let fixture: ComponentFixture<BookComponent>;
+  users: User[];
+  user: String;
+  first_name: String;
+  last_name: String;
+  phone_number: String;
+  appartment_amount: String;
+  date_from: String;
+  date_to: String;
+  
+  
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BookComponent ]
-    })
-    .compileComponents();
-  }));
+  constructor(private userService: UserService) { 
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BookComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    this.userService.getUsers()
+    .subscribe(users =>{
+      this.users = users;
+    });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  }
+
+  addUser(event){
+    event.preventDefault();
+    var newUser = {
+      first_name: this.first_name,
+      last_name: this.last_name,
+       phone_number: this. phone_number,
+      appartment_amount: this.appartment_amount,
+      date_from: this.date_from,
+      date_to: this.date_to
+
+    }
+
+    this.userService.addUser(newUser)
+    .subscribe(user =>{
+
+      this.users.push(user);
+      this.first_name ="";
+      this.last_name ="";
+      this.phone_number ="";
+      this.date_from ="";
+      this.date_to="";
+    });
+  }
+
+
+  deleteUser(id){
+		var users = this.users;
+
+		this.userService.deleteUser(id).subscribe(data =>{
+			if(data.n == 1){
+				for(var i = 0;i< users.length; i++){
+					if(users[i]._id ==id){
+						users.splice(i, 1);
+					}
+				}
+			}
+		});
+  }
+  
+  // updateUser(user){
+  //   var _user = {
+  //     _id: user._id,
+  //     first_name: user.first_name,
+  //     last_name: user.last_name,
+  //     specialty: user.specialty,
+  //     likes: user.likes
+  //   };
+
+  //   this.userService.updateUser(_user).subscribe(data =>{
+  //     user.first_name = user.first_name,
+  //     user.last_name = user.last_name,
+  //     user.specialty = user.specialty,
+  //     user.likes = user.likes
+  //   })
+         
+    
+  // }
+
+  ngOnInit() {
+  }
+
+}
+
